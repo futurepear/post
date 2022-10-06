19bd7f25b44c6c6044ac1afdbf47f1db5f4ff7a3
pyrus pear <99107938+futurepear@users.noreply.github.com>
2022-10-06 20:28:05 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const express = require("express");
+const grape = require("./grape");
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const server = http.createServer(app);
+const branch = grape.setupBranch(server);
+
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//catch body parser errors
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err);
+  } else {
+    next()
+  }
+});
+
+console.log(branch.id);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/a.html");
+});
+
+app.post('/', (req, res) => {
+  console.log('post' + JSON.stringify(req.body));
+  res.end(JSON.stringify({ a: 'a' }));
+});
